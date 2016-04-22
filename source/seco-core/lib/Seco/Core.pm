5c0b915fbb231bcc1119b2c0cf8ee20df346835f
tingle2008 <tingle2008@gmail.com>
2016-04-22 20:12:20 +0800

    seco aws init

---
+++ source/seco-core/lib/Seco/Core.pm
@@ -0,0 +1,54 @@
+=head1 NAME
+
+Seco::Core - stub
+
+=head1 SYNOPSIS
+
+use Seco::Core; # Waste Perl Time
+
+=head1 DESCRIPTION
+
+This is a stub for Ext::MakeMaker
+
+See also
+
+  Seco::Maint::Helper
+  Seco::MultiPhasic
+  Seco::Node::Purpose
+  Seco::Pony::Animate
+  Seco::Pony::Say
+  Seco::SSH::External::Result
+  Seco::SSH::External
+  Seco::Accessor
+  Seco::SwitchInfo::MacScraper
+  Seco::SwitchInfo::PortStatus
+  Seco::Class
+  Seco::YRT3::Lite
+  Seco::WhoismyDataCenter
+  Seco::MultipleCmd
+  Seco::MultipleSsh
+  Seco::MultipleTcp
+  Seco::Pony
+  Seco::Proxy
+  Seco::Range
+  Seco::RunAs
+  Seco::Slogan
+  Seco::Core
+  Seco::fakeroot
+  Seco::sudo
+ 
+
+=cut
+
+package Seco::Core;
+
+use Exporter;
+use vars qw(@ISA @EXPORT_OK @EXPORT $VERSION);
+
+@ISA = qw/Exporter/;
+@EXPORT_OK = ();
+%EXPORT_TAGS = ();
+@EXPORT = ();
+$VERSION = "1.0.0";
+
+1;
